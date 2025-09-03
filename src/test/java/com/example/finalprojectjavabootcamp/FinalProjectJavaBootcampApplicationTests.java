61aafc8e4abcd7b12d6fab082432c196aa57a614
Essa <essaalghinm@gmail.com>
2025-09-03 23:25:37 +0300

    Initial commit

---
+++ src/test/java/com/example/finalprojectjavabootcamp/FinalProjectJavaBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectjavabootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectJavaBootcampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

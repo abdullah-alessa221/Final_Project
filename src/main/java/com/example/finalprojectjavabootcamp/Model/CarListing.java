8ae94a53e95cabefdda8e8f77dfb39a1bd9f1948
Essa <essaalghinm@gmail.com>
2025-09-04 23:31:59 +0300

    Added Listing, CarListing, RealEstateListing, and Rating

---
+++ src/main/java/com/example/finalprojectjavabootcamp/Model/CarListing.java
@@ -0,0 +1,38 @@
+package com.example.finalprojectjavabootcamp.Model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CarListing {
+
+    @Id
+    private Integer id;
+
+    private String car_type; // suv, sedan... etc
+    private String year;
+    private String mileage;
+    private String make;
+    private String model;
+    private String color;
+    private String fuel_type;
+    private String city;
+
+
+    @OneToOne
+    @MapsId
+    @JsonIgnore
+    private Listing listing;
+
+}
---
+++ src/main/java/com/example/finalprojectjavabootcamp/Service/CarListingService.java
@@ -0,0 +1,4 @@
+package com.example.finalprojectjavabootcamp.Service;
+
+public class CarListingService {
+}

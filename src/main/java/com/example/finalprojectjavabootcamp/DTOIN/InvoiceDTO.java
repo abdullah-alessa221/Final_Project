762c8a7c11fc19db4eabe7c57b1bd303714f4e31
Essa <essaalghinm@gmail.com>
2025-09-09 10:24:18 +0300

    added deep search and fixed prop file

--- src/main/java/com/example/finalprojectjavabootcamp/DTOIN/InvoiceDTO.java
+++ src/main/java/com/example/finalprojectjavabootcamp/DTOIN/InvoiceDTO.java
@@ -9,6 +9,9 @@ import lombok.Setter;
 @AllArgsConstructor
 public class InvoiceDTO {
 
+    private String id;
+    private String carModel;
+    private String carYear;
     private String buyerName;
     private String buyerPhone;
     private String sellerName;
